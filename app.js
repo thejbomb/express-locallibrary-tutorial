ec3c6923b02746beb325b74604ec86377dacbfcb
Jun <junkun007@aim.com>
2019-01-15 21:55:39 -0500

    Added files and ahcnes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://jun:159aobp@ds255784.mlab.com:55784/local_library';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://jun:159aobp@ds255784.mlab.com:55784/local_library';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
